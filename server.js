e5753b4e3617b1f6c362d5a66e8f74b72de89b2b
Mrnikifabio <fabiocrugnola2000@gmail.com>
2020-10-04 14:51:21 +0200

    fix double names
    Fix a problem that caused a bug in case of people with more than a name

--- server.js
+++ server.js
@@ -79,14 +79,14 @@ app.post('/login',(req, res)=>{
                 {
                     var user = (await (await driver).findElement(webdriver.By.css("div.ibe_util_texte.ibe_actif"))).getText();
                     user = (await user).split(" - ")[1].split(" ");
-                    var name = user[1];
+                    var name = user[user.length-2];
                     var surname = user[0];
 
                     //get student class
 
                     if(opt == "std"){
                         var _class = "";
-                        _class = user[2].replace("(","").replace(")","");
+                        _class = user[user.length-1].replace("(","").replace(")","");
                     }
 
                     res.setHeader('Content-Type', 'application/json');
